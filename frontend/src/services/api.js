23c3409a3c05e945aad0b27727b5940fea534a66
coderzlyf <coderzlyf@gmail.com>
2026-05-06 15:37:12 +0530

    connect to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://task-board-ai-vrvh.onrender.com/",
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    req.headers.Authorization = token;
+    req.headers.Authorization = `Bearer ${token}`;
   }
 
   return req;
