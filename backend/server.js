dbd3d9576f0bf5f5b785d9213151fa9528b5546f
coderzlyf <coderzlyf@gmail.com>
2026-05-06 16:20:00 +0530

    fix http cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 // Socket setup
 const io = new Server(server, {
   cors: {
-    origin: "https://task-board-ai-six.vercel.app",
+    origin: "*",
   },
 });
 
